b9486d1adc2760031b96255ba3a8b88afc2ad52a
dimou-john <dimoujohnprivate@gmail.com>
2018-06-30 15:07:20 +0300

    Checkout back-end getting ready

---
+++ server/src/main/java/com/devticket/model/ticket/OrderLines.java
@@ -0,0 +1,79 @@
+package com.devticket.model.ticket;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "ORDERSLINES")
+public class OrderLines{
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+//    @ManyToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "order_id", nullable = false)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    @JsonIgnore
+    private Orders orderid;
+
+
+
+    @Column(name = "TICKETID")
+    private Long ticketId;
+
+    @Column(name = "QUANTITY")
+    private int quantity;
+
+
+    @Column(name = "PRICE")
+    private float price;
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Orders getOrder() {
+        return orderid;
+    }
+
+    public void setOrder(Orders order) {
+        this.orderid = orderid;
+    }
+
+    public Long getTicketId() {
+        return ticketId;
+    }
+
+    public void setTicketId(Long ticketId) {
+        this.ticketId = ticketId;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+
+}
---
+++ server/src/main/java/com/devticket/model/ticket/Orders.java
@@ -0,0 +1,55 @@
+package com.devticket.model.ticket;
+
+import javax.persistence.*;
+import java.util.Calendar;
+
+@Entity
+@Table(name = "ORDERS")
+public class Orders {
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "date")
+    private Calendar date;
+
+
+    @Column(name = "userid")
+    private Long userId;
+
+    @Column(name = "totalprice")
+    private float totalPrice;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Calendar getDate() {
+        return date;
+    }
+
+    public void setDate(Calendar date) {
+        this.date = date;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public float getTotalPrice() {
+        return totalPrice;
+    }
+
+    public void setTotalPrice(float totalPrice) {
+        this.totalPrice = totalPrice;
+    }
+}
