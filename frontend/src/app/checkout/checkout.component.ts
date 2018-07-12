ecf579e208553d3547fb26736a1b4a7e45313ecf
Mike George Verros <mixalisverros@hotmail.gr>
2018-07-12 19:32:40 +0300

    checjout

--- frontend/src/app/checkout/checkout.component.ts
+++ frontend/src/app/checkout/checkout.component.ts
@@ -55,12 +55,19 @@ export class CheckoutComponent implements OnInit {
 
   }
 
-  async checkout(content) {
-    await this.makeUser(JSON.stringify(this.UserService.currentUser));
-    delay(3000);
-    this.CartService.checkout(this.CurrentUserId);
-    this.CartService.clearCart();
-    this.modalService.open(content, { size: 'lg' });
+  async commitOrder() {
+    this.makeUser(JSON.stringify(this.UserService.currentUser));
+  }
+
+  checkout(content) {
+    this.commitOrder()
+      .then( () => {
+      delay(3000);
+      this.CartService.checkout(this.CurrentUserId);
+      this.CartService.clearCart();
+      this.modalService.open(content, { size: 'lg' });
+    })
+    .catch(() => alert('We apologise something went wrong') );
   }
 
 
